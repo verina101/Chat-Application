ab17a016be0ab840254027f4f33833281c292d74
verina.gad <verinagad258@gmail.com>
2022-04-16 14:11:02 +0200

    Add project files.

---
+++ Chat Application/Chat Application.cpp
@@ -0,0 +1,12 @@
+﻿// Chat Application.cpp : Defines the entry point for the application.
+//
+
+#include "Chat Application.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
