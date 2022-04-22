2e0eec4bf56c6bc77519d04222c3d972b309a487
Amir Moris <amir.moris35@gmail.com>
2022-04-22 20:48:14 +0200

    tables modified

--- Chat-Application/Chat-Application.cpp
+++ Chat-Application/Chat-Application.cpp
@@ -7,7 +7,7 @@
 #include <map>
 using namespace std;
 
-void printselected(vector<map<string, string>>& vec) {
+void PrintSelectedData(vector<map<string, string>>& vec) {
 	for (auto it : vec) {
 		for (auto itt : it) {
 			cout << itt.first << " " << itt.second << "\n";
@@ -19,29 +19,6 @@ void printselected(vector<map<string, string>>& vec) {
 int main() {
 	Data test; // create your database 
 	test.CreateTables(); // create all tables
-	/*
-	
-	string tablename = "CHATROOM";
-	string columns = "(Name, DESCREPTION)";
-	string values = "('Kebol3dewya', 'ht3ml eh ya w7sh')";
-	test.InsertData(tablename, columns, values);
 
 
-	columns = "(Name, DESCREPTION)";
-	values = "('AdelShakal', '6 ela tlt')";
-	test.InsertData(tablename, columns, values);
-
-
-	string newval = "Name = 'haaaey'";
-	string cond = "WHERE ID = '1'";
-	test.UpdateData(tablename, newval, cond);
-
-
-	cond = "WHERE ID = '1'";
-	test.DeleteData(tablename, cond);
-
-	string selectcol = "*";
-	vector<map<string, string>> vec = test.SelectData(tablename, selectcol);
-	printselected(vec);
-	*/
 }
