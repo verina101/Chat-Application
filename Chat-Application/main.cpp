8c14a1c713acbc3b8241a97d8e96cf9f3f3ca578
verina.gad <verinagad258@gmail.com>
2022-04-29 01:50:38 +0200

    empty commit

--- Chat-Application/main.cpp
+++ Chat-Application/main.cpp
@@ -20,16 +20,6 @@ int main(int argc, char *argv[]) {
     Data MyDataBase; // create your database
     MyDataBase.CreateTables(); // create all tables
 
-    //create users
-    //(FirstName, LastName, ProfilePicture, Description, Password, PhoneNumber, Visibility)
-    string tableName = "USER";
-    string columns = "*";
-    string values = "('Maria', 'Tawfek', 'NA', 'Available', 'abcd', '01478523699', 'f')";
-    string empty = "";
-    //MyDataBase.InsertData(tableName, columns, values);
-
-    vector<vector<QString>> vec = MyDataBase.SelectData(tableName, columns, empty);
-    MyDataBase.DisplayData(vec);
 
     return APP.exec();
 }
